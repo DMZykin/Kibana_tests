56c62f953fd086993abbacf96e7380ca3e59a75d
Gaidunov Vasiliy <gaidunov18@gmail.com>
2024-07-16 13:41:20 +0500

    Add 2836614d.py

---
+++ 2836614d.py
@@ -0,0 +1,28 @@
+try:
+    for i in range(2):
+        print(i)
+except Exception as e:
+    print(e)
+
+try:
+    if o > 3:
+        print('o is greater than 3')
+except Exception as e:
+    print(e)
+
+class Impact(Near):
+    def __init__(self, arg2):
+        self.value = 8
+
+def nice(arg3, arg2, arg1):
+    print('Develop defense majority too information tonight.')
+
+for i in range(7):
+    result = 3 + 3
+
+class Former(Throw):
+    def __init__(self, arg3):
+        print('Loop iteration')
+
+for i in range(10):
+    print('Loop iteration')
